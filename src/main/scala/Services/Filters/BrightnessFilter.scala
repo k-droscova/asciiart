919ff905ade1240ff79cf215720818d00ba68c5f
Karolina Droscova <drosckar@fit.cvut.cz>
2024-10-04 22:59:45 +0200

    basic filter implementation

--- src/main/scala/Services/Filters/BrightnessFilter.scala
+++ src/main/scala/Services/Filters/BrightnessFilter.scala
@@ -1,5 +1,27 @@
 package Services.Filters
 
-class BrightnessFilter {
+import Core.Models.{Pixel, Image}
 
+/**
+ * A filter that adjusts the brightness of the image.
+ *
+ * @param brightness the brightness adjustment value (-255 to 255).
+ */
+case class BrightnessFilter(brightness: Int) extends Filter {
+
+  /**
+   * Applies the brightness filter to the given image.
+   * Delegates the brightness adjustment to the pixel's `adjustBrightness` method.
+   *
+   * @param image the image to adjust brightness.
+   * @return the brightness-adjusted image.
+   */
+  override def apply(image: Image[? <: Pixel]): Image[? <: Pixel] = {
+    val newPixels = image.pixels.map { row =>
+      row.map { pixel =>
+        pixel.adjustBrightness(brightness)
+      }
+    }
+    new Image(image.width, image.height, newPixels)
+  }
 }
--- src/main/scala/Services/Filters/Filter.scala
+++ src/main/scala/Services/Filters/Filter.scala
@@ -1,5 +1,15 @@
 package Services.Filters
 
+import Core.Models.{Pixel, Image}
+/**
+ * Trait representing a filter that can be applied to an image.
+ */
 trait Filter {
-
+  /**
+   * Applies the filter to the given image.
+   *
+   * @param image the image to which the filter is applied.
+   * @return the filtered image.
+   */
+  def apply(image: Image[? <: Pixel]): Image[? <: Pixel]
 }
