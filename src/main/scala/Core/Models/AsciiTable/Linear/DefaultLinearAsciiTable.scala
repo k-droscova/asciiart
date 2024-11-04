92caddc997ba38b072b16a143dd7b155a5deb165
Karolina Droscova <drosckar@fit.cvut.cz>
2024-11-04 18:24:55 +0100

    docs

--- src/main/scala/Core/Models/AsciiTable/Linear/DefaultLinearAsciiTable.scala
+++ src/main/scala/Core/Models/AsciiTable/Linear/DefaultLinearAsciiTable.scala
@@ -1,3 +1,14 @@
 package Core.Models.AsciiTable.Linear
 
+/**
+ * DefaultLinearAsciiTable provides a preset ASCII table for converting grayscale
+ * values to ASCII characters. It maps grayscale values (0-255) to characters
+ * `" .:-=+*#%@"` in a linear way, from darkest (`' '`) to brightest (`'@'`).
+ *
+ * Example:
+ * {{{
+ *   val table = new DefaultLinearAsciiTable()
+ *   table.getAsciiCharacter(128) // Returns '='
+ * }}}
+ */
 class DefaultLinearAsciiTable extends LinearAsciiTable(" .:-=+*#%@")
--- src/main/scala/Core/Models/AsciiTable/Linear/LinearAsciiTable.scala
+++ src/main/scala/Core/Models/AsciiTable/Linear/LinearAsciiTable.scala
@@ -2,9 +2,28 @@ package Core.Models.AsciiTable.Linear
 
 import Core.Models.AsciiTable.AsciiTable
 
+/**
+ * LinearAsciiTable maps grayscale values (0-255) to ASCII characters linearly. This class is abstract,
+ * so different ASCII styles can be set in subclasses by changing the characters used.
+ *
+ * @param characters A string of ASCII characters for mapping. Each character represents a section
+ *                   of the grayscale range.
+ *
+ * Example:
+ * {{{
+ *   val table = new DefaultLinearAsciiTable()
+ *   table.getAsciiCharacter(128) // Returns the ASCII char for mid-range grayscale
+ * }}}
+ */
 abstract class LinearAsciiTable(val characters: String) extends AsciiTable {
   private val range: Int = Math.ceil(256.0 / characters.length).toInt
 
+  /**
+   * Gets the ASCII character for a given grayscale value.
+   *
+   * @param grayscale Grayscale value (0-255)
+   * @return ASCII character that best represents this grayscale
+   */
   def getAsciiCharacter(grayscale: Int): Char = {
     characters(Math.min(grayscale / range, characters.length - 1))
   }
