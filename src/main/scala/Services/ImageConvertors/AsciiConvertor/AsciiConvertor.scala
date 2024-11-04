cd6b8ed2bcd62b2266f9d8ba1e02f48881c585c2
Karolina Droscova <drosckar@fit.cvut.cz>
2024-11-04 18:49:56 +0100

    abstract class for ascii convertors

---
+++ src/main/scala/Services/ImageConvertors/AsciiConvertor/AsciiConvertor.scala
@@ -0,0 +1,31 @@
+package Services.ImageConvertors.AsciiConvertor
+
+import Core.Models.AsciiTable.AsciiTable
+import Core.Models.Image.{AsciiImage, GrayscaleImage}
+import Core.Models.Pixel.AsciiPixel
+import Services.ImageConvertors.ImageConvertor
+
+
+/**
+ * Abstract class for converting a GrayscaleImage to an AsciiImage using a specified AsciiTable.
+ *
+ * @param asciiTable The table that maps grayscale intensities to ASCII characters.
+ */
+abstract class AsciiConvertor(asciiTable: AsciiTable) extends ImageConvertor[GrayscaleImage, AsciiImage] {
+
+  /**
+   * Converts a GrayscaleImage to an AsciiImage by mapping each pixel's intensity to an ASCII character.
+   *
+   * @param input The GrayscaleImage to convert.
+   * @return The resulting AsciiImage.
+   */
+  override def convert(input: GrayscaleImage): AsciiImage = {
+    val asciiPixels = input.pixels.map { row =>
+      row.map { pixel =>
+        val asciiChar = asciiTable.getAsciiCharacter(pixel.intensity)
+        AsciiPixel(asciiChar)
+      }
+    }
+    AsciiImage(asciiPixels)
+  }
+}
