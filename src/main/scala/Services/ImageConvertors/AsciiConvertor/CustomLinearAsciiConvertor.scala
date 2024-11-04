1e25c969c6af92b40a7beda51cbf3fcfe5c3507f
Karolina Droscova <drosckar@fit.cvut.cz>
2024-11-04 18:56:02 +0100

    custom linear ascii convertor + tests

--- src/main/scala/Services/ImageConvertors/AsciiConvertor/CustomLinearAsciiConvertor.scala
+++ src/main/scala/Services/ImageConvertors/AsciiConvertor/CustomLinearAsciiConvertor.scala
@@ -1,5 +1,5 @@
 package Services.ImageConvertors.AsciiConvertor
 
-class CustomLinearAsciiConvertor {
+import Core.Models.AsciiTable.Linear.CustomLinearAsciiTable
 
-}
+class CustomLinearAsciiConvertor(customChars: String) extends AsciiConvertor(new CustomLinearAsciiTable(customChars))
